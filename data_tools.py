504994435c2691189a9fdfa808a91b24451bd912
Kerby Shedden <kshedden@umich.edu>
2018-07-29 10:49:12 -0400

    update

--- data_tools.py
+++ data_tools.py
@@ -2,17 +2,17 @@ import pandas as pd
 from config import *
 
 # Read the cohort file
-pa = "/nfs/kshedden/Beverly_Strassmann/Cohort_2018.csv.gz"
+pa = "/nfs/kshedden/Beverly_Strassmann/Cohort_2018_072818.csv.gz"
 df = pd.read_csv(pa)
 
 # Modify some of the variable names
 df["Sex"] = df["Sex"].replace({0: "Female", 1: "Male"})
 df["Female"] = df["Sex"].replace({"Female": 1, "Male": 0})
 df["Age"] = df.Age_Yrs
-df["HAZ"] = df.HAZ_17
-df["BAZ"] = df.BAZ_15
-df["WAZ"] = df.WAZ_15
-df["HT"] = df["Ht_Ave"]
+df["HAZ"] = df.HAZ_18
+df["BAZ"] = df.BAZ_18
+df["WAZ"] = df.WAZ_18
+df["HT"] = df["Ht_Ave_18"]
 
 # Use data one year beyond the imputation range.
 df = df.loc[df.Age <= maxage + 1, :]
